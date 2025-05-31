6c5f3c44617d5c1d6fafbe5491adf1266c35ee68
Pirraa <pirrafranci2004@gmail.com>
2025-05-31 13:53:47 +0200

    commit di prova

--- progetto_biblioteca/strumenti/connect.php
+++ progetto_biblioteca/strumenti/connect.php
@@ -3,7 +3,8 @@
 
     try{
 		// Connessione -> db_ip, db_username, db_password, db_name
-        $link = mysqli_connect("localhost", "user", "password", "Biblioteca"); 
+        $link = mysqli_connect("localhost", "francesco", "password", "Biblioteca"); 
+        //$link = mysqli_connect("localhost", "user", "password", "Biblioteca"); 
     } catch(mysqli_sql_exception $e) {
 		die("Non posso stabilire la connessione al db: " . $e->getMessage());
 	}
