8e35e25710c205c02e0d35deae90a4051783605d
MatteoFormenton <148442431+MatteoFormenton@users.noreply.github.com>
2025-05-31 16:17:19 +0200

    Update index.php

--- progetto_biblioteca/index.php
+++ progetto_biblioteca/index.php
@@ -1,15 +1,11 @@
-<?php include("strumenti/navbar.php"); ?>
 
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Pagina di Prova</title>
 </head>
 <body>
-    <h1>Benvenuto nella pagina di prova!</h1>
-
     <?php
-    echo "<p>Oggi è " . date("d/m/Y") . "</p>";
+ include("utenti.php");
     ?>
 </body>
 </html>
